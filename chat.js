50c4a258fd864d16aa0da640fcf5fb0de61f0a41
Diego J. Q. DelCarpio <diego.g20x@gmail.com>
2019-12-10 17:07:02 -0500

    validacion login usuario

--- chat.js
+++ chat.js
@@ -6,49 +6,57 @@ var PORT = process.env.PORT || 8080;
 var users = []
     ident = [];
 
-app.get('/',function(req,res){
-    //request : son cabeceras y datos que nos envia el navegador.
-    //response : son todo lo que enviamos desde el servidor.
+app.get('/',function(req,res)
+{
+    // request : son cabeceras y datos que nos envia el navegador.
+    // response : son todo lo que enviamos desde el servidor.
     res.sendFile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function(socket){
-    socket.on('login', function(data) {
-        console.log('a user ' + data.userId + ' connected');
-        users.push(data.userId);
-        ident.push(socket.id);
+io.sockets.on('connection', function(socket)
+{
+    socket.on('login', function(data)
+    {
+        // console.log('a user ' + data.userId + ' connected');
+        if (users.indexOf(data.userId) !== -1)
+        {
+            // console.log('data');
+        } else {
+            users.push(data.userId);
+            ident.push(socket.id);
+        }
         io.emit('login', users, data.userId);
     });
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function()
+    {
         var id = ident.indexOf(socket.id);
-        console.log(id + ' | user ' + users[id] + ' disconnected');
-        if (id >= 0) {
+        // console.log(id + ' | user ' + users[id] + ' disconnected');
+        if (id >= 0)
+        {
             users.splice(id, 1);
             ident.splice(id, 1); 
         }
         io.emit('disconnect', users);
     });
 
-    socket.on('notificacion', function(datos){
+    socket.on('notificacion', function(datos)
+    {
         io.emit('notificar',datos);
     });
 
-    socket.on('respuestaNotificacion', function(datos){
+    socket.on('respuestaNotificacion', function(datos)
+    {
         io.emit('respuesta',datos);
     });
 
-    socket.on('alertaUsuario', function(datos){
+    socket.on('alertaUsuario', function(datos)
+    {
         io.emit('notificarUsuario',datos);
     });
 
-    /* SOCKET MYHOUSE */
-    
-    socket.on('pruebadingo', function(datos){
-        io.emit('pruebadingob',datos);
-    })
-
-    socket.on('enviarMensaje', function(datos) {
+    socket.on('enviarMensaje', function(datos)
+    {
        io.emit('mensajeUsuario',datos); 
     });
 
